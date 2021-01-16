4c15a621306a35d6945f35deeb88d39d23428653
Christian Koopmann <christian@Christians-MacBook-Pro.local>
2021-01-16 15:12:29 +0100

    Adjust setup script to assume pulled directories

--- setup.sh
+++ setup.sh
@@ -5,11 +5,11 @@ brew install libpng
 
 BASEDIR="$PWD"
 echo "Exporting CXX and LDFLAGS"
-export CXXFLAGS="-nostdinc++ -isystem$BASEDIR/CppND-Route-Planning-Project/thirdparty/clang/include/c++/v1"
+export CXXFLAGS="-nostdinc++ -isystem$BASEDIR/thirdparty/clang/include/c++/v1"
 export LDFLAGS="$LDFLAGS -L/opt/X11/lib -L/opt/local/lib -L/usr/local/lib"
-export LDFLAGS="-L$BASEDIR/CppND-Route-Planning-Project/thirdparty/clang/lib -Wl,-rpath,$BASEDIR/CppND-Route-Planning-Project/thirdparty/clang/lib"
+export LDFLAGS="-L$BASEDIR/thirdparty/clang/lib -Wl,-rpath,$BASEDIR/thirdparty/clang/lib"
 
-cd CppND-Route-Planning-Project/thirdparty
+cd "thirdparty"
 
 echo "Download modified headers"
 curl -O "https://releases.llvm.org/6.0.0/clang+llvm-6.0.0-x86_64-apple-darwin.tar.xz"
@@ -19,13 +19,15 @@ rm clang+llvm-6.0.0-x86_64-apple-darwin.tar.xz
 
 echo "Download / Install io2p"
 cd P0267_RefImpl
+sudo rm -rf Debug
 mkdir Debug && cd Debug
 cmake --config Debug "-DCMAKE_BUILD_TYPE=Debug" -DIO2D_DEFAULT=COREGRAPHICS_MAC ..
 cmake --build .
 sudo make install
 
 echo "Build the main project"
-cd "$BASEDIR/CppND-Route-Planning-Project"
+cd "$BASEDIR"
+rm -rf build
 mkdir build && cd build
 cmake ..
 make
