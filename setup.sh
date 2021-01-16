cc7def758b798fa5be4290704a01bade459f9187
Christian Koopmann <christian@Christians-MacBook-Pro.local>
2021-01-16 14:32:44 +0100

    Setup script to automate building / installing dependencies on mac

---
+++ setup.sh
@@ -0,0 +1,38 @@
+brew install cmake
+brew install libpng
+
+echo "Download modified headers"
+git clone --recurse-submodules https://github.com/ckoopmann/CppND-Route-Planning-Project.git
+BASEDIR="$PWD"
+
+echo "Exporting CXX and LDFLAGS"
+export CXXFLAGS="-nostdinc++ -isystem$BASEDIR/CppND-Route-Planning-Project/thirdparty/clang/include/c++/v1"
+export LDFLAGS="$LDFLAGS -L/opt/X11/lib -L/opt/local/lib -L/usr/local/lib"
+export LDFLAGS="-L$BASEDIR/CppND-Route-Planning-Project/thirdparty/clang/lib -Wl,-rpath,$BASEDIR/CppND-Route-Planning-Project/thirdparty/clang/lib"
+
+cd CppND-Route-Planning-Project/thirdparty
+
+echo "Download modified headers"
+curl -O "https://releases.llvm.org/6.0.0/clang+llvm-6.0.0-x86_64-apple-darwin.tar.xz"
+tar -xf clang+llvm-6.0.0-x86_64-apple-darwin.tar.xz
+mv clang+llvm-6.0.0-x86_64-apple-darwin clang
+rm clang+llvm-6.0.0-x86_64-apple-darwin.tar.xz
+
+echo "Download / Install io2p"
+git clone --recurse-submodules https://github.com/cpp-io2d/P0267_RefImpl
+cd P0267_RefImpl
+mkdir Debug && cd Debug
+cmake --config Debug "-DCMAKE_BUILD_TYPE=Debug" -DIO2D_DEFAULT=COREGRAPHICS_MAC ..
+cmake --build .
+sudo make install
+
+echo "Build the main project"
+cd "$BASEDIR/CppND-Route-Planning-Project"
+mkdir build && cd build
+cmake ..
+make
+
+echo "Build the main project"
+./OSM_A_star_search
+
+
